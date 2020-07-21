0c531274738358aced83d63de012155ae05b2d1f
Serpentiel <hello@serpentiel.me>
2020-07-22 00:20:59 +0300

    Added Docker image files

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /home/container
+sleep 1
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+# Update Don't Starve Together Server
+./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update 343050 +quit
+
+cd bin
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
